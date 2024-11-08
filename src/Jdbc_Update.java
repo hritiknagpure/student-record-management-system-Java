fc30f31c88b8c90c5eca7fac20fb6f7861406bc8
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-11-08 16:13:57 +0530

    Jdbc_Update.java

--- src/Jdbc_Update.java
+++ src/Jdbc_Update.java
@@ -7,39 +7,32 @@ import java.sql.Statement;
 public class Jdbc_Update {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String value;
 		int eid;
 		double esal;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "scott", "tiger");
-			Statement smt= con.createStatement();
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yourDatabaseName", "root", "@hritik");
+			Statement smt = con.createStatement();
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-				System.out.print("enter Emp no: ");
-				value=br.readLine();
-//				than covert
-				eid=Integer.parseInt(value);
-				System.out.print("enter Emp new salary: ");
-				value=br.readLine();
-				esal=Double.parseDouble(value);
-				int count = smt.executeUpdate("update empp set esal="+esal+" where eno="+eid);
-				if (count > 0)
-					System.out.println(count + " rows updated");
-				else
-					System.out.println("no record forund");
-				
-			}
-
-		catch(
-
-	Exception e)
-	{
-		// TODO: handle exception
-		System.out.print(e);
+			System.out.print("Enter Emp ID: ");
+			value = br.readLine();
+			eid = Integer.parseInt(value); 
+			System.out.print("Enter Emp new salary: ");
+			value = br.readLine();
+			esal = Double.parseDouble(value);
+			int count = smt.executeUpdate("UPDATE empp SET esal=" + esal + " WHERE eno=" + eid);
+			
+			// Print the result
+			if (count > 0)
+				System.out.println(count + " rows updated");
+			else
+				System.out.println("No record found");
+			
+			// Close the connection
+			con.close();
+		} catch (Exception e) {
+			System.out.print(e);
+		}
 	}
-
-}
-
 }
