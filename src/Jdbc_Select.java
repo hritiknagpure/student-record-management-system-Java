7d6383078e24afc1e945cb85c4a98792acd1f166
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-11-08 16:10:58 +0530

    Jdbc_Select.java

--- src/Jdbc_Select.java
+++ src/Jdbc_Select.java
@@ -6,27 +6,28 @@ import java.sql.Statement;
 public class Jdbc_Select {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
-			Statement smt=con.createStatement();
-			ResultSet rs=smt.executeQuery("select * from empp");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yourDatabaseName", "hritik", "@hritik");
+	                //to execute the string querry
+			Statement smt = con.createStatement();
+			ResultSet rs = smt.executeQuery("SELECT * FROM empp");
 			while(rs.next()) {
-				int eno=rs.getInt(1);
-				String ename=rs.getString(2);
-				Double esal=rs.getDouble(3);
-				System.out.println("Emp Id"+eno);
-				System.out.println("Emp Ename"+ename);
-				System.out.println("Emp Esal"+esal);
-				System.out.println("\n");
+				int eno = rs.getInt(1);
+				String ename = rs.getString(2);
+				double esal = rs.getDouble(3);
+				
+				System.out.println("Emp Id: " + eno);
+				System.out.println("Emp Name: " + ename);
+				System.out.println("Emp Salary: " + esal);
+				System.out.println();
 			}
+			
+			// Close the connection
+			con.close();
 		}
 		catch (Exception e) {
-			// TODO: handle exception
 			System.out.print(e);
 		}
-
 	}
-
 }
